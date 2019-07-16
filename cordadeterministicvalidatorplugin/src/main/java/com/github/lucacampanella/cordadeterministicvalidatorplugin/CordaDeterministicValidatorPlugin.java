f66e44c7d9c9cb29c9e08b08ac533d3171b33833
Luca Campanella <luca.campanella1@gmail.com>
2019-07-16 10:33:54 +0200

    Update versions, task name...

--- cordadeterministicvalidatorplugin/src/main/java/com/github/lucacampanella/cordadeterministicvalidatorplugin/CordaDeterministicValidatorPlugin.java
+++ cordadeterministicvalidatorplugin/src/main/java/com/github/lucacampanella/cordadeterministicvalidatorplugin/CordaDeterministicValidatorPlugin.java
@@ -16,6 +16,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class CordaDeterministicValidatorPlugin implements Plugin<Project> {
@@ -65,7 +66,10 @@ public class CordaDeterministicValidatorPlugin implements Plugin<Project> {
                 final File javac = project.file(jdkHome.getPath() + "/bin/javac");
                 fileWriter = new FileWriter(javac, false);
                 fileWriter.write("#!/bin/sh\necho \"javac 1.8.0_172\"\n");
-                javac.setExecutable(true, false);
+                final boolean setExecRes = javac.setExecutable(true, false);
+                if(!setExecRes) {
+                    throw new GradleException("Could not set javac as an executable");
+                }
 
             } catch (IOException e) {
                 throw new GradleException("Could not write to file", e);
@@ -75,37 +79,48 @@ public class CordaDeterministicValidatorPlugin implements Plugin<Project> {
 
 
         final JavaCompile javaDeterministicCompileTask = project.getTasks()
-                .create("javaDeterministicCompile", JavaCompile.class);
+                .create("compileDeterministicJava", JavaCompile.class);
         final Configuration deterministicImplementation = project.getConfigurations().create("deterministicImplementation");
 
         javaDeterministicCompileTask.dependsOn(installJdkTask);
-        javaDeterministicCompileTask.getOptions().setCompilerArgs(Arrays.asList("-bootclasspath", rtJar.getPath()));
+        final List<String> compilerArgs = javaDeterministicCompileTask.getOptions().getCompilerArgs();
+        if(!compilerArgs.contains("-parameters")) {
+            compilerArgs.add("-parameters");
+        }
+        if(compilerArgs.contains("-bootclasspath")) {
+            final int indexOfBoothClasspath = compilerArgs.indexOf("-bootclasspath");
+            compilerArgs.remove(indexOfBoothClasspath);
+            compilerArgs.remove(indexOfBoothClasspath);
+        }
+        compilerArgs.add("-bootclasspath");
+        compilerArgs.add(rtJar.getPath());
+        javaDeterministicCompileTask.getOptions().setCompilerArgs(compilerArgs);
 
         project.afterEvaluate(prog -> {
 
             final JavaCompile javaCompileTask = (JavaCompile) project.getTasks().getByName("compileJava");
 
-        deterministicImplementation.extendsFrom(project.getConfigurations().getByName("compileClasspath"));
+            deterministicImplementation.extendsFrom(project.getConfigurations().getByName("compileClasspath"));
 
-        substitute(deterministicImplementation, "corda-serialization", DEFAULT_CORDA_VERSION);
-        substitute(deterministicImplementation, "corda-core", DEFAULT_CORDA_VERSION);
+            substitute(deterministicImplementation, "corda-serialization", DEFAULT_CORDA_VERSION);
+            substitute(deterministicImplementation, "corda-core", DEFAULT_CORDA_VERSION);
 
-        javaDeterministicCompileTask.setClasspath(deterministicImplementation);
-        javaDeterministicCompileTask.setSource(javaCompileTask.getSource());
+            javaDeterministicCompileTask.setClasspath(deterministicImplementation);
+            javaDeterministicCompileTask.setSource(javaCompileTask.getSource());
 
-        final File outputDir = project.file("build/deterministic");
-        outputDir.mkdirs();
-        javaDeterministicCompileTask.setDestinationDir(outputDir);
-    });
+            final File outputDir = project.file("build/deterministic");
+            outputDir.mkdirs();
+            javaDeterministicCompileTask.setDestinationDir(outputDir);
+        });
 
-   }
+    }
 
-   private static Optional<Dependency> getDependency(Configuration configuration, String cordaDepName) {
-       return configuration.getAllDependencies()
-               .stream()
-               .filter(dep -> dep.getName().contains(cordaDepName) &&
-                       dep.getGroup().equals(CORDA_RELAESE_GROUP)).findAny();
-   }
+    private static Optional<Dependency> getDependency(Configuration configuration, String cordaDepName) {
+        return configuration.getAllDependencies()
+                .stream()
+                .filter(dep -> dep.getName().contains(cordaDepName) &&
+                        dep.getGroup().equals(CORDA_RELAESE_GROUP)).findAny();
+    }
 
     private void substitute(Configuration configuration, String cordaDepName, String defaultVers) {
         final DependencySubstitutions depSub = configuration.getResolutionStrategy().getDependencySubstitution();
@@ -127,6 +142,7 @@ public class CordaDeterministicValidatorPlugin implements Plugin<Project> {
         }
 
         depSub.substitute(depSub.module(CORDA_RELAESE_GROUP + ":" + cordaDepName))
-                .with(depSub.module(CORDA_RELAESE_GROUP + ":" + cordaDepName + DETERMINISTIC_IDENTIFIER + ":" + cordaVersion));
+                .with(depSub.module(CORDA_RELAESE_GROUP + ":" + cordaDepName + DETERMINISTIC_IDENTIFIER + ":"
+                        + cordaVersion));
     }
 }
